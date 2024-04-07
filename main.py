c738c7bfc554cc599a0cd99f126c26066428db47
Melanie-RK <caafpiraat@gmail.com>
2024-04-07 10:02:24 +0200

    feat: Persist quotes channel data
    The name of the quote channel is now persisted. You no longer have to
    set the quotes channel each time the bot restarts.

--- main.py
+++ main.py
@@ -1,10 +1,29 @@
 import random
+import pymongo
 import discord
 from discord.ext import commands
+
+QUOTES_CHANNEL = ""
 
 TOKEN = ""
 with open('token.txt') as f:
     TOKEN = f.readline().strip('\n')
+
+DB_USER = ""
+with open('db_user.txt') as f:
+    DB_USER = f.readline().strip('\n')
+
+DB_PASSWORD = ""
+with open('db_pw.txt') as f:
+    DB_PASSWORD = f.readline().strip('\n')
+
+CLUSTERSTRING = ""
+with open('clusterstring.txt') as f:
+    CLUSTERSTRING = f.readline().strip('\n')
+
+client = pymongo.MongoClient(f"mongodb+srv://{DB_USER}:{DB_PASSWORD}@{CLUSTERSTRING}/?retryWrites=true&w=majority&appName=NimueBot")
+db = client.discord
+quotes_channels = db.quotes_channels
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -16,15 +35,41 @@ async def on_ready():
     print(f'Logged in as {bot.user} (ID: {bot.user.id})')
     print('------')
 
+
 @bot.command()
 async def set_quotes_channel(ctx, arg):
     global QUOTES_CHANNEL
     QUOTES_CHANNEL = arg
+    print(QUOTES_CHANNEL)
+
+    if QUOTES_CHANNEL:
+        # Store quotes channel information in MongoDB
+        get_from_database = quotes_channels.find_one({'server': ctx.guild.id})
+
+        if get_from_database is not None:
+            quotes_channels.update_one({"server": ctx.guild.id}, {"$set": {"channel_name": arg}}, upsert=True)
+        else:                              
+            channel_entry = {
+                'server': ctx.guild.id,
+                'channel_name': QUOTES_CHANNEL
+            }
+            quotes_channels.insert_one(channel_entry)
+
     await ctx.send(f'Quotes channel set to {arg}')
+
+async def get_quotes_channel(ctx):
+    get_from_database = quotes_channels.find_one({'server': ctx.guild.id})
+    channel_name = get_from_database['channel_name']
+   
+    return channel_name
 
 @bot.command()
 async def random_quote(ctx):
     global QUOTES_CHANNEL
+
+    if QUOTES_CHANNEL == '':
+        QUOTES_CHANNEL = await get_quotes_channel(ctx)       
+    
     try:
         channel = discord.utils.get(ctx.guild.channels, name=QUOTES_CHANNEL)
         messages = [message async for message in channel.history(limit=100)] #limit the bot to the last 100 messages
