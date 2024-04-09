07366b533d8c97c7498d67c2f358515df462bed6
Melanie <caafpiraat@gmail.com>
2024-04-09 23:06:01 +0200

    feat: Add more insults to the bot
    Because it's funny

---
+++ vicious_mockery.py
@@ -0,0 +1,31 @@
+import random
+
+vicious_mockery = [
+    "Your mother was a hamster and your father smells of elderberries",
+    "One day you will meet a doppleganger of yourself and be disappointed",
+    "On a scale of 1 - 10, you're proper screwed",
+    "I didn't know challenge ratings went into the negatives",
+    "Believe in yourself because no one else does",
+    "I'd love to help you out.. Which way did you come in",
+    "You could starve a succubus...",
+    "Some people have a stick up their ass, you have the whole tree",
+    "Clotpole",
+    "You could be out-witted by a rat",
+    "I fart in your general direction. Now go away or I shall taunt you a second time",
+    "You aren't important enough for a specific insult",
+    "I envy people that have never met you",
+    "If you were an inanimate object, you would be a participation trophy",
+    "You make me wish I had more middle fingers",
+    "If laughter is the best medicine, your face must be curing the world",
+    "As monsters go, you're the least frightening scarecrow that I've ever seen!",
+    "You have more dick in your personality than you do in your pants",
+    "I hope your wife brings a date to your funeral",
+    "Why are you playing hard to get when you are so hard to want",
+    "Ah, now here's a specimin with a charm truly rivaled only by that of their own intellect",
+    "Honestly, is this the life your mother wanted for you",
+    "I must commend your courage, it takes a brave soul to show up disarmed to a duel of wits",
+    "I have two minutes to kill, tell me everything you know" 
+]
+
+def random_insult():
+    return random.choice(vicious_mockery)
